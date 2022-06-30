e81b066a55e3fb3efff70c067517f2d0cff8a0f7
kylecooltron <kylejcoulon@gmail.com>
2022-06-30 11:58:33 -0600

    added comments to laser and control actors action

--- gamename/game/casting/laser.py
+++ gamename/game/casting/laser.py
@@ -7,7 +7,7 @@ class Laser(Actor):
     """
     A Laser
 
-    The responsibility of Laser is to delete itself when offscreen.
+    The responsibility of Laser is to move and delete itself when offscreen.
 
     Attributes:
 
@@ -19,15 +19,18 @@ class Laser(Actor):
 
     def move_next(self):
         """Moves the actor to its next position according to its velocity. Will wrap the position 
-        from one side of the screen to the other when it reaches the given maximum x (X ONLY FOR LASER).
+        from one side of the screen to the other when it reaches the given maximum (X ONLY FOR LASER).
 
         Args:
             max_x (int): The maximum x value.
         """
+        # determine movement (wrap x)
         x = (self._position.get_x() + self._velocity.get_x()) % constants.MAX_X
         y = (self._position.get_y() + self._velocity.get_y())
-
+        # if laser goes off screen
         if y <= 0:
+            # delete it
             self._cast.remove_actor("lasers", self)
         else:
+            # apply movement
             self._position = Point(x, y)
